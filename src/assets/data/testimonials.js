b79abcf5cdd24812ba3e16a9395e5be0bca38bda
Enuel <enuel.monzon@gmail.com>
2022-01-24 20:28:45 -0300

    Primer commit

---
+++ src/assets/data/testimonials.js
@@ -0,0 +1,27 @@
+const testimonials = [
+  {
+    id: 1,
+    name: 'Leonel Magallanes',
+    title: 'System analyst',
+    org: 'Befaust',
+    desc:
+      'Enuel is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
+  },
+  {
+    id: 2,
+    name: 'William Cortés',
+    title: 'Front-End Developer',
+    org: 'Beti',
+    desc:
+      'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
+  },
+  {
+    id: 3,
+    name: 'Cristián Ferreyra',
+    title: 'Back-End Developer',
+    org: 'Autodidact',
+    desc:
+      'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
+  },
+];
+export default testimonials;
---
+++ src/pages/Projects.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Projects = () => (
+  <div>
+    <h1>This is Projects Page</h1>
+  </div>
+);
+
+export default Projects;
