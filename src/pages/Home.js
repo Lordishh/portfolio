d44def7ece67275b0be0de9646c82828b148b8c2
Enuel <enuel.monzon@gmail.com>
2022-01-25 07:22:53 -0300

    service & project section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
 import HeroSection from '../components/HeroSection';
+import ProjectsSection from '../components/ProjectsSection';
+import ServiceSection from '../components/ServiceSection';
 
 const Home = () => (
   <div>
     <HeroSection />
     <AboutSection />
+    <ServiceSection />
+    <ProjectsSection />
   </div>
 );
 
