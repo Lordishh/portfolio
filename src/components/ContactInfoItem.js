4a294ca242fb220fbb38537558618a3a80277184
Enuel <enuel.monzon@gmail.com>
2022-01-26 08:56:23 -0300

    Contact Page

--- src/components/ContactInfoItem.js
+++ src/components/ContactInfoItem.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { MdPlace } from 'react-icons/md';
+import styled from 'styled-components';
+import PText from './PText';
 
-const ContactInfoItem = () => (
-  <div>
-    <h1>contact info</h1>
-  </div>
+const ItemStyles = styled.div`
+  padding: 2rem;
+  background-color: var(--deep-dark);
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  border-radius: 8px;
+  margin-bottom: 2rem;
+  .icon {
+    color: var(--white);
+    background-color: var(--gray-2);
+    padding: 1.3rem;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+  }
+  svg {
+    width: 3.5rem;
+  }
+`;
+
+const ContactInfoItem = ({ icon = <MdPlace />, text = 'this is an info' }) => (
+  <ItemStyles>
+    <div className="icon">{icon}</div>
+    <div className="info">
+      <PText>{text}</PText>
+    </div>
+  </ItemStyles>
 );
 
 export default ContactInfoItem;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ContactSection from '../components/ContactSection';
+import Map from '../components/Map';
 
 const Contact = () => (
   <div>
     <ContactSection />
+    <Map />
   </div>
 );
 
