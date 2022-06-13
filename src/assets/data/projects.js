f1dc19b81f748c32b3aa14f37a4c42f17107f7d2
Enuel <enuel.monzon@gmail.com>
2022-06-13 02:47:43 -0300

    fixes projects

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import GoldenQueen from '../images/GoldenQueen.png';
+import GoldQueen from '../images/GoldQueen.png';
 import CrudApp from '../images/CrudApp.png';
 import GifExpert from '../images/GifExpert.png';
 import Renacer from '../images/Renacer.png';
@@ -9,14 +9,47 @@ import animationCss from '../images/animation-css.png';
 import huddle from '../images/huddle.png';
 import heroesApp from '../images/heroes-app.png';
 import fixnet from '../images/fixnet.png';
+import blogr from '../images/blogr.png';
 
 const projects = [
   {
     id: uuidv4(),
-    name: 'GoldenQueen',
+    name: 'Renacer',
+    desc:
+      'Renacer es una aplicación web de para un centro de pilates. Para realizarlo utilicé React, Styled Componets y Figma.',
+    img: Renacer,
+    linkCode: 'https://renacer.netlify.app/',
+  },
+  {
+    id: uuidv4(),
+    name: 'Blogr',
+    desc:
+      'Blogr es un desafío "Landing Page" de FrontendMentor, para realizarlo utilicé Smeantic HTML5, CSS Grid, Flexbox y Mobile-first.',
+    img: blogr,
+    linkCode: 'https://enu-blogr.netlify.app',
+  },
+  {
+    id: uuidv4(),
+    name: 'Sunnyside',
+    desc:
+      'Sunnyside es un desafío "Landing Page" de FrontendMentor, para realizarlo utilicé Smeantic HTML5, CSS Grid y Mobile-first.',
+    img: sunnyside,
+    linkCode: 'https://enudev-sunnyside.netlify.app',
+  },
+  {
+    id: uuidv4(),
+    name: 'Heroes App',
+    desc:
+      'Heroes App es una aplicación web hecha en con React dónde trabajo con reducers, context, y hooks. También utilizo manejo de rutas con React Router',
+    img: heroesApp,
+    linkCode: 'https://enu-heroes.netlify.app/',
+  },
+  {
+    id: uuidv4(),
+    name: 'GoldQueen',
     desc:
       'GoldQueen es un diseño web responsivo de un restaurante, el mismo se adapta a la resolución de cualquier dispositivo',
-    img: GoldenQueen,
+    img: GoldQueen,
     linkCode: 'https://goldqueen.netlify.app/',
   },
   {
@@ -29,41 +62,9 @@ const projects = [
   },
   {
     id: uuidv4(),
-    name: 'Heroes App',
-    desc:
-      'Heroes App es una aplicación web hecha en con React dónde trabajo con reducers, context, y hooks. También utilizo manejo de rutas con React Router',
-    img: heroesApp,
-    linkCode: 'https://enu-heroes.netlify.app/',
-  },
-  {
-    id: uuidv4(),
-    name: 'Renacer',
-    desc:
-      'Renacer es una página para un centro de pilates. La misma es un desarrollo responsivo con la función modo oscuro',
-    img: Renacer,
-    linkCode: 'https://renacer.netlify.app/',
-  },
-  {
-    id: uuidv4(),
-    name: 'CRUD-App',
-    desc:
-      'CrudApp es una aplicación hecha con React que su función es que cada usuario pueda guardar notas personales - Sin desplegar',
-    img: CrudApp,
-    linkCode: 'https://github.com/Lordishh/CRUD-app',
-  },
-  {
-    id: uuidv4(),
-    name: 'Sunnyside',
-    desc:
-      'Sunnyside es un desafío "Landing Page" de FrontendMentor, para realizarlo utilicé CSS Grid Layout.',
-    img: sunnyside,
-    linkCode: 'https://enudev-sunnyside.netlify.app',
-  },
-  {
-    id: uuidv4(),
     name: 'Room',
     desc:
-      'Room es un desafío "Landing Page" de FrontendMentor, para realizarlo utilicé CSS Grid Layout.',
+      'Room es un desafío "Landing Page" de FrontendMentor, para realizarlo utilicé Smeantic HTML5, CSS Grid y Mobile-first.',
     img: room,
     linkCode: 'https://enudev-roompage.netlify.app',
   },
@@ -71,7 +72,7 @@ const projects = [
     id: uuidv4(),
     name: 'Animation CSS',
     desc:
-      'El objetivo de Animation CSS es mostrar animaciones con CSS vanilla, un código sencillo y elegante.',
+      'El objetivo de Animation CSS es mostrar animaciones con CSS vanilla.',
     img: animationCss,
     linkCode: 'https://images-animation.netlify.app',
   },
@@ -79,9 +80,9 @@ const projects = [
     id: uuidv4(),
     name: 'Huddle',
     desc:
-      'Huddle es un desafío "Landing Page" de FrontendMentor, para realizarlo utilicé CSS Flex Layout.',
+      'Blogr es un desafío "Landing Page" de FrontendMentor, para realizarlo utilicé Smeantic HTML5, Flexbox y Mobile-first.',
     img: huddle,
-    linkCode: 'https://enueldev-huddle.netlify.appp',
+    linkCode: 'https://enueldev-huddle.netlify.app/',
   },
   {
     id: uuidv4(),
@@ -89,6 +90,14 @@ const projects = [
     desc: 'Fixnet fue uno de mis primeros diseños web antes de utilizar BEM.',
     img: fixnet,
     linkCode: 'https://fixnet.netlify.app/',
+  },
+  {
+    id: uuidv4(),
+    name: 'CRUD-App',
+    desc:
+      'CrudApp es una aplicación hecha con React que su función es que cada usuario pueda guardar notas personales (No está en producción)',
+    img: CrudApp,
+    linkCode: 'https://github.com/Lordishh/CRUD-app',
   },
 ];
 
